ef4f305aa2e5550dabdc31ede2055ecbf931913a
Manas Kumar Maji <manasmaji4@gmail.com>
2021-03-16 12:26:28 +0530

    Initial Commit

---
+++ packages/test1/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    printHello: function() {console.log("Hello");},
+    printHi: function() {console.log("Hi");},
+};
---
+++ packages/test3/index.js
@@ -0,0 +1,3 @@
+const test1 = require("@manasmaji/test1");
+
+test1.printHello();
